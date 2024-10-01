18fe234621c5d159caff44c64c63b98460bffdd6
EcosystemNetwork.io <86893562+EcosystemNetwork@users.noreply.github.com>
2024-09-30 19:24:05 -0700

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -4,18 +4,30 @@ import { Box } from '@mui/material';
 const Footer = ({ src, alt, width, height }) => {
   return (
     <Box
-      component="img"
-            href="https://www.lionscre.com/"
+      component="a"
+      href="https://www.lionscre.com/"
       target="_blank"
       rel="noopener noreferrer"
-      src="/License.png" // Image located in public folder
-      alt={alt || 'License Image'} // Default alt text
       sx={{
-        width: width || '100%', // Default width to 100% if not provided
-        height: height || '100%', // Default height to auto if not provided
-        objectFit: 'cover', // Ensure the image fits nicely
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        padding: 0,
+        margin: 0,
       }}
-    />
+    >
+      <Box
+        component="img"
+        src={src || '/License.png'} // Use dynamic src or fallback to default
+        alt={alt || 'License Image'} // Default alt text
+        sx={{
+          width: width || '100%', // Allow width to be passed or default to 100%
+          height: height || 'auto', // Allow height to be passed or default to auto
+          objectFit: 'cover', // Ensure the image fits nicely
+        }}
+      />
+    </Box>
   );
 };
 
