e95298d64026d733b60d777537148c2dc96e6990
EcosystemNetwork.io <86893562+EcosystemNetwork@users.noreply.github.com>
2024-09-30 19:21:03 -0700

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -1,52 +1,22 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Box } from '@mui/material';
 
 const Footer = ({ src, alt, width, height }) => {
   return (
     <Box
-      component="a"
-      href="https://www.lionscre.com/"
+      component="img"
+            href="https://www.lionscre.com/"
       target="_blank"
       rel="noopener noreferrer"
+      src="/License.png" // Image located in public folder
+      alt={alt || 'License Image'} // Default alt text
       sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: 'auto',
-        height: 'auto',
-        padding: 0, // Removed padding to eliminate extra space
-        margin: 0,  // Ensure there's no margin
+        width: width || '100%', // Default width to 100% if not provided
+        height: height || '100%', // Default height to auto if not provided
+        objectFit: 'cover', // Ensure the image fits nicely
       }}
-    >
-      <Box
-        component="img"
-        src={src || '/License.png'}
-        alt={alt || 'License Image'}
-        sx={{
-          width: width || 'auto',
-          height: height || 'auto',
-          maxWidth: '100%',
-          objectFit: 'contain',
-        }}
-      />
-    </Box>
+    />
   );
 };
 
-Footer.propTypes = {
-  src: PropTypes.string,
-  alt: PropTypes.string,
-  width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-};
-
-Footer.defaultProps = {
-  src: '/License.png',
-  alt: 'License Image',
-  width: 'auto',
-  height: 'auto',
-};
-
 export default Footer;
-
